d05928d2b2b4cd3fe7a21d9af0cec27289954fdc
Anton <antoninkersev@gmail.com>
2020-09-04 17:28:00 +0300

    ref posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,25 +1,54 @@
 class PostsController < ApplicationController
+    before_action :authenticate_user!
+
     def new
         @post = Post.new        
     end
+
+    def show
+        @posts = Post.where('author = ?', current_user.id).order(created_at: :desc)
+        @followers = User.joins("INNER JOIN followers ON users.id = followers.followed_user WHERE follower = #{current_user.id} ")
+        render 'welcome/mypage'
+    end
+    
+    
     
     def create
-        @post = Post.new
-        @post.title = params[:title]
+        @post = Post.new(post_params)
         @post.author = current_user.id
-        @post.content = params[:content]
-        @post.pic = params[:pic]
         if @post.save
             redirect_to controller: "welcome", action: "mypage"
         else
             render 'new'
         end
     end
+
+    def edit
+        @post = Post.find(params[:id])
+    end
+    
+
+    def update
+        @post = Post.find(params[:id])
+        if @post.author == current_user.id
+            if @post.update(post_params)
+                redirect_to @post
+            else 
+                render 'edit'
+            end
+        else
+            render plain: 'error!'
+        end
+    end
     
     def destroy
-        post = Post.find(params[:id])
-        post.destroy
-        redirect_back fallback_location: '/'
+        @post = Post.find(params[:id])
+        if @post.author == current_user.id
+            @post.destroy
+            redirect_back fallback_location: '/'  
+        else
+            render plain: 'error!'
+        end
     end
 
     def like
@@ -36,6 +65,10 @@ class PostsController < ApplicationController
         redirect_back fallback_location: '/'
     end
     
+    private
+    def post_params
+        params.require(:post).permit(:title, :content, :pic )
+    end
     
     
 end
